ac9dce7d04cd963085ec118845b2647db9c0251a
x0tero <gxoelotero@gmail.com>
2024-09-09 04:51:09 +0200

    anomaly details

---
+++ microservizos/anomalyModule/anomalias.Dockerfile
@@ -0,0 +1,6 @@
+FROM python:3
+RUN mkdir /home/inputs
+RUN pip install --no-cache-dir --upgrade pip && \
+    pip install --no-build-isolation --no-cache-dir python-csv==0.0.10 paho-mqtt pandas numpy joblib scikit-learn
+
+CMD ["cat", "/etc/os-release"]
--- microservizos/anomalyModule/anomalyDetection.py
+++ microservizos/anomalyModule/anomalyDetection.py
@@ -1,40 +1,71 @@
-from pymongo import MongoClient
 import paho.mqtt.client as mqtt
 import os, datetime
 import pandas as pd
+import joblib
+import numpy as np
 
-topicSub = "topics/station1/irradiation/raw"
-topicPub = "topics/station1/irradiation/processed"
+topicSubIrradiation = "topics/station1/irradiation/raw"
+topicSubPower = "topics/station1/power/raw"
+topicSubTemperature = "topics/station1/temperature/raw"
+
+topicPubIrradiation = "topics/station1/irradiation/processed"
+topicPubPower = "topics/station1/power/processed"
+topicPubTemperature = "topics/station1/temperature/processed"
 format_date = "%Y:%m:%dT%H:%M:%S"
 clientMQTT = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 
 
+listTemperature = []
+listPower = []
+listIrradiation = []
+model = joblib.load('/home/model/isolation_forest_model.pkl')
+
+
 def subscribe(client: mqtt):
-    # Se recibimos mensaxe pasamos o modelo de anomalias e publicamos
+    # Se recibimos mensaxe cargamos o modelo de anomalias e publicamos
     def on_message(clientMQTT, userdata, msx):
-        #data = msx.payload.decode().replace(" ","")
+        #data = msx.kpayload.decode().replace(" ","")
+        topic = msx.topic
         data = msx.payload.decode()
         fecha = data.split(",")[0]
-        irradiacion = float(data.split(",")[1])
-        clientMQTT.publish(topicPub, df.to_string())
-        with open('model/isolation_forest_model.pkl', 'rb') as file:
-            dt_object = datetime.strptime(datetime_string, "%Y-%m-%d %H:%M:%S")
-            hora = dt_object.hour
-            mes = dt_object.month
-            X = pd.DataFrame({
-                'Mes': [mes],
-                'Hora': [hora],
-                'G(i)': [irradiacion]
-            })
-            model = pickle.load(file)
-            Y = model.predict(X)
-            print("dato: " + str(irradiacion) + "  anomalia: " + str(Y))
-    
-    clientMQTT.subscribe(topicSub)
+
+        if topic == topicSubIrradiation:
+            irradiacion = float(data.split(",")[1])
+            listIrradiation.append((fecha,irradiacion))
+        elif topic == topicSubPower:
+            power = float(data.split(",")[1])
+            listPower.append((fecha,power))
+        elif topic == topicSubTemperature:
+            temperature = float(data.split(",")[1])
+            listTemperature.append((fecha,temperature))
+
+        if listTemperature and listPower and listIrradiation:
+            # Compruebas si recibiste todos los datos e realizas a deteccion de anomalias
+            if (fecha == listTemperature[-1][0]) and (fecha == listPower[-1][0]) and(fecha == listIrradiation[-1][0]):
+                x = np.array([[listTemperature[-1][1], listPower[-1][1], listIrradiation[-1][1]]])
+                res = model.predict(x)
+                print("Anomalo dato (temp: " + str(listTemperature[-1][1]) + ", potencia: " + str(listPower[-1][1]) + ", irradiacion: " + str(listIrradiation[-1][1]) + ") = " + str(res))
+                if res[0] == -1:
+                    msxI = str(fecha) + ",-1"
+                    msxP = str(fecha) + ",-1"
+                    msxT = str(fecha) + ",-1"
+                else:
+                    msxI = str(fecha) + "," + str(listIrradiation[-1][1])
+                    msxP = str(fecha) + "," + str(listPower[-1][1])
+                    msxT = str(fecha) + "," + str(listTemperature[-1][1])
+                clientMQTT.publish(topicPubIrradiation, msxI)
+                clientMQTT.publish(topicPubPower, msxP)
+                clientMQTT.publish(topicPubTemperature, msxT)
+            
+
+            
+    clientMQTT.subscribe(topicSubIrradiation)
+    clientMQTT.subscribe(topicSubPower)
+    clientMQTT.subscribe(topicSubTemperature)
     clientMQTT.on_message = on_message
 
 
 if __name__ == '__main__':
     clientMQTT.connect('172.18.0.99', 1883)
-    subscribe(client)
+    subscribe(clientMQTT)
     clientMQTT.loop_forever()
